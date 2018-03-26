951eb0ca5c46cff7688f4e0d0aab20e4792134e0
Mike <mbcacciano@gmail.com>
2018-03-25 22:34:16 -0400

    animated skill bar section complete but not responsive

--- public/js/progressBar.js
+++ public/js/progressBar.js
@@ -1,21 +1,8 @@
-function progress() {
-    var prg = document.getElementById('progress');
-    var percent = document.getElementById('percentCount');
-    var counter = 5;
-    var progress = 25;
-    var id = setInterval(frame, 25);
-
-    function frame() {
-        if(counter == 100) {
-            clearInterval(id);
-        } else {
-            progress += 4;
-            counter += 1;
-            prg.style.width = progress + 'px';
-            percent.innerHTML = counter + '%';
-        }
-    }
-    
-}
-
-progress();
+$(document).ready(function() {
+    $('html').animate({width:'70%'}, 3000);
+    $('css').animate({width:'60%'}, 3000);
+    $('sass').animate({width:'50%'}, 3000);
+    $('js').animate({width:'60%'}, 3000);
+    $('resp').animate({width:'70%'}, 3000);
+    $('node').animate({width:'30%'}, 3000);
+});
--- public/js/stickyNav.js
+++ public/js/stickyNav.js
@@ -2,6 +2,13 @@ $(document).ready(function() {
     $('.menu-icon').on('click', function() {
         $('nav ul').toggleClass('showing');
     });
+
+    $('.html').animate({width:'70%'}, 3000);
+    $('.css').animate({width:'60%'}, 3000);
+    $('.sass').animate({width:'50%'}, 3000);
+    $('.js').animate({width:'60%'}, 3000);
+    $('.resp').animate({width:'70%'}, 3000);
+    $('.node').animate({width:'30%'}, 3000);
 });
 
 $(window).on('scroll', function() {
