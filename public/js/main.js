d5a94ed894dc7d3707020573ed0ede2f6e5afa64
Mike <mbcacciano@gmail.com>
2018-03-27 02:28:00 -0400

    90% responsive added scrollReveal

--- public/js/main.js
+++ public/js/main.js
@@ -9,6 +9,111 @@ $(document).ready(function() {
     $('.js').animate({width:'60%'}, 3000);
     $('.resp').animate({width:'80%'}, 3000);
     $('.node').animate({width:'40%'}, 3000);
+
+    window.sr = ScrollReveal({ reset: true });
+    // NAV
+    sr.reveal('.leftSlide', {
+        duration: 2000,
+        distance: '50px',
+        origin: 'left',
+        mobile: true,
+        reset: false
+    });
+    // NAV
+    sr.reveal('.rightSlide', {
+        duration: 2000,
+        distance: '50px',
+        origin: 'right',
+        mobile: true,
+        reset: false
+    });
+    // HEADER TITLE
+    sr.reveal('.headerName', {
+        duration: 800,
+        delay: 600,
+        origin: 'top',
+        mobile: true,
+        scale: 0.5,
+        easing: 'ease-in',
+        opacity: 0,
+        rotate: { x: 0, y: 150, z: 0}
+    });
+    // HEADER PARAGRAPH
+    sr.reveal('.headerP', {
+        duration: 900,
+        delay: 900,
+        origin: 'bottom',
+        mobile: true,
+        scale: 0.5,
+        easing: 'ease-in',
+        opacity: 0,
+        rotate: { x: 200, y: 0, z: 0 }
+    });
+    // READ MORE BTN
+    sr.reveal('.btn', {
+        duration: 1000,
+        delay: 1500,
+        distance: '50px',
+        origin: 'bottom',
+        mobile: true,
+        easing: 'ease-in',
+        opacity: 0
+    });
+    // SECTION TITLES
+    sr.reveal('.sectionTitle', {
+        duration: 1000,
+        distance: '50px',
+        origin: 'top',
+        mobile: true,
+        easing: 'ease-in',
+    });
+    // ABOUT CARDS
+    sr.reveal('.about1', {
+        duration: 1200,
+        distance: '50px',
+        origin: 'top',
+        mobile: true,
+        easing: 'ease-in',
+        rotate: { x: 0, y: 150, z: 0 }
+    });
+    sr.reveal('.about2', {
+        duration: 1200,
+        distance: '50px',
+        origin: 'top',
+        mobile: true,
+        easing: 'ease-in',
+        rotate: { x: 150, y: 0, z: 0 }
+    });
+    sr.reveal('.about3', {
+        duration: 1200,
+        distance: '50px',
+        origin: 'top',
+        mobile: true,
+        easing: 'ease-in',
+        rotate: { x: 0, y: -150, z: 0 }
+    });
+    sr.reveal('.progress-bar', {
+        duration: 1200,
+        distance: '50px',
+        origin: 'right',
+        mobile: true,
+        easing: 'ease-in'
+    });
+    sr.reveal('.proj1', {
+        duration: 1000,
+        distance: '80px',
+        origin: 'left',
+        mobile: true,
+        easing: 'ease-in'
+    });
+    sr.reveal('.proj2', {
+        duration: 1000,
+        distance: '80px',
+        origin: 'right',
+        mobile: true,
+        easing: 'ease-in'
+    });
+
 });
 
 $(window).on('scroll', function() {
@@ -17,5 +122,5 @@ $(window).on('scroll', function() {
     } else {
         $('nav').removeClass('black');
     }
-})
+});
 
